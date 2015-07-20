6da48e2bc0e1b098b8805fa38a177bdf35d1054c
Andrew Mendoza <Amend035@gmail.com>
2015-07-19 20:21:08 -0400

    creating super admin sees everything student does

--- app/controllers/app.controller.js
+++ app/controllers/app.controller.js
@@ -75,12 +75,10 @@ app.directive('createAccount', [function() {
 			});
 
 			$scope.create = function(register) {
-				if(register.role == 'student') {
-                    register.email = register.email + "@" + register.school.u_emaildomain;
-					register.u_id = register.school.u_id;
-				} else {
-                    register.u_id = -1;
-                }
+				
+                register.email = register.email + "@" + register.school.u_emaildomain;
+				register.u_id = register.school.u_id;
+				
 
 				User.resource.save(register, function(response) {
                     Session.create(response);
